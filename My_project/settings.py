39540b084bb61aebdca6e34d35266f6ceaf77cd7
g-an24 <pratham.sharma6113@gmail.com>
2023-02-11 01:14:10 -0500

    allowed path

--- My_project/settings.py
+++ My_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3*l4*9_e-bvu^dnlh^tt@zqs2fqm$5&r4jzmrrdc-f!f*!&+p1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
